16804acc65e2eb6ba4899489aa46ee6405399cc6
Nicolai Weitkemper <nico.weio@gmail.com>
2019-11-01 11:43:15 +0100

    Aufgabe 3_2 hinzugefügt

---
+++ Blatt 3/Aufgabe_03_2.cpp
@@ -0,0 +1,130 @@
+/*** Aufgabe_03_2.cpp ***/
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int tag, monat, jahr;
+  cout << "Geben Sie Tag, Monat und Jahr ein " << endl;
+  cin >> tag;
+  cin >> monat;
+  cin >> jahr;
+  // cout << "Eingabe: " << tag << "." << monat << "." << jahr << endl;
+  // *************************************
+
+  bool isSchaltjahr = ((jahr % 400 == 0) || ((jahr % 4 == 0) && (jahr % 100 != 0)));
+
+  bool isDayValid = (tag > 0); //wird im Folgenden weiter bestimmt
+  bool isMonthValid = (monat > 0 && monat <= 12);
+  bool isYearInBounds = (jahr > 1600 && jahr < 2600);
+
+  switch (monat) {
+  case 1: //Januar
+  case 3: //März
+  case 5: //Mai
+  case 7: //Juli
+  case 8: //August
+  case 10: //Oktober
+  case 12: //Dezember
+    {
+      //31 Tage
+      if (tag > 31) {
+        isDayValid = false;
+      }
+      break;
+    }
+  case 4: //April
+  case 6: //Juni
+  case 9: //September
+  case 11: //November
+    {
+      //30 Tage
+      if (tag > 30) {
+        isDayValid = false;
+      }
+      break;
+    }
+  case 2: //Februar
+    {
+      //SONDERFALL: 28 oder 29 Tage
+      if (isSchaltjahr ? (tag > 29) : (tag > 28)) {
+        isDayValid = false;
+      }
+    }
+  }
+
+  if (!isDayValid || !isMonthValid || !isYearInBounds) {
+    cout << "Naechster Tag ist 99.99.9999" << endl;
+    return 1;
+  }
+
+  //******************
+
+  tag++;
+
+  switch (monat) {
+  case 1: //Januar
+  case 3: //März
+  case 5: //Mai
+  case 7: //Juli
+  case 8: //August
+  case 10: //Oktober
+  case 12: //Dezember
+    {
+      //31 Tage
+      if (tag > 31) {
+        tag = 1;
+        monat++;
+      }
+      break;
+    }
+  case 4: //April
+  case 6: //Juni
+  case 9: //September
+  case 11: //November
+    {
+      //30 Tage
+      if (tag > 30) {
+        tag = 1;
+        monat++;
+      }
+      break;
+    }
+  case 2: //Februar
+    {
+      //SONDERFALL
+      if (isSchaltjahr ? (tag > 29) : (tag > 28)) {
+        tag = 1;
+        monat++;
+      }
+    }
+  }
+
+  //TODO Schaltjahr hier wichtig!?
+  if (monat > 12) {
+    monat = 1;
+    jahr++;
+    cout << "Monat > 12" << endl;
+  }
+
+  // *************************************
+  cout << "Naechster Tag ist " <<
+    tag << "." << monat << "." << jahr << endl;
+  return 0;
+}
+/* Ausgabe:
+0.11.2016:
+Naechster Tag ist 99.99.9999
+
+11.11.1111:
+Naechster Tag ist 99.99.9999
+
+30.11.2016:
+Naechster Tag ist 1.12.2016
+
+28.02.2016:
+Naechster Tag ist 29.2.2016
+
+28.02.2017:
+Naechster Tag ist 1.3.2017
+ */
+/*** Ende Aufgabe_03_2.cpp ***/
